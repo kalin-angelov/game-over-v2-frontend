19f816ea5af741f06fe0d886dc5d5e903215f5b6
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-04-07 18:59:36 +0300

    Create RouteGuard.jsx

---
+++ src/components/reusable/routeGuard/RouteGuard.jsx
@@ -0,0 +1,22 @@
+import { Outlet, useNavigate } from "react-router-dom";
+
+import { useContext, useEffect } from "react";
+
+import { AuthContext } from "../../../contexts/AuthContext"; 
+
+export const RouteGuard = () => {
+    const navigate = useNavigate();
+    const { isAuthenticated } = useContext(AuthContext);
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            navigate("/register");
+        }
+
+        if (isAuthenticated) {
+            navigate("/profile")
+        }
+    },[]);
+
+    return <Outlet/>
+};
