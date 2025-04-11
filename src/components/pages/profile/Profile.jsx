d8d5955e04747a1734f16a17a69f56e05c0cc498
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-04-11 15:22:29 +0300

    Update Profile.jsx

--- src/components/pages/profile/Profile.jsx
+++ src/components/pages/profile/Profile.jsx
@@ -1,17 +1,94 @@
+import styles from "./Profile.module.css";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 import { AuthContext } from "@/contexts/AuthContext";
+import { useUserStore } from "@/store/user";
+
+import { FaFolderOpen } from "react-icons/fa";
+import { BsPlusSquare } from "react-icons/bs";
 
 const Profile = () => {
+    const {userInfo, setUserInfo} = useContext(AuthContext);
+    const { getUser } = useUserStore();
+    const [defaultUserInfo, setDefaultUserInfo] = useState({
+        username: "",
+        email: "orel40@gmail.com",
+        profileImg: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    })
 
-    const { userInfo } = useContext(AuthContext);
-    console.log(userInfo);
+    useEffect(() => {
+
+        getUser()
+            .then(data => console.log(data))
+            .catch(error => console.error(error))
+
+    },[userInfo]);
     
+    const editProfileInfo = (e) => {
+        e.preventDefault;
+
+        toast.success("Profile successfully edit.");
+    }
 
     return (
-        <main>
-            <h1>Welcome: {userInfo.email} to your Profile page</h1>
+        <main className={styles.profile}>
+            
+            <div className={styles.profileCard}>
+                <figure className={styles.profileImgContainer}>
+                    <img className={styles.defaultProfileImg} src={defaultUserInfo.profileImg} alt="profile img" />
+                </figure>
+
+                <p>My profile</p>
+
+                <form className={styles.editProfileForm}>
+
+                    <input type="text" placeholder="Username" value={defaultUserInfo.username} onChange={(e) => setDefaultUserInfo({...defaultUserInfo, username: e.target.value})}/>
+                    <input type="text" value={defaultUserInfo.email} onChange={(e) => setDefaultUserInfo({...defaultUserInfo, email: e.target.value})}/>
+                    <input type="text" placeholder="Profile Img" value={defaultUserInfo.profileImg} onChange={(e) => setDefaultUserInfo({...defaultUserInfo, profileImg: e.target.value})}/>
+
+                    <button className={styles.editProfileBtn} type="submit" onClick={(e) => editProfileInfo(e)}>Edit</button>
+                </form>
+            </div>
+
+            <div className={styles.userGamesContainer}>
+
+                <div className={styles.addGameContainer}>
+                    <BsPlusSquare fontSize={30}/>
+                </div>
+
+                <div className={styles.gameContainer}>
+                    <figure className={styles.gameCoverContainer}>
+                        <img className={styles.gameImg} src="https://upload.wikimedia.org/wikipedia/en/7/77/Assassins_Creed_2_Box_Art.JPG" alt="game cover" />
+                    </figure>
+
+                    <p>AC 2</p>
+
+                    <FaFolderOpen fontSize={30}/>
+                </div>
+
+                <div className={styles.gameContainer}>
+                    <figure className={styles.gameCoverContainer}>
+                        <img className={styles.gameImg} src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b5/God_of_War_%282005%29_cover.jpg/250px-God_of_War_%282005%29_cover.jpg" alt="game cover" />
+                    </figure>
+
+                    <p>God of war</p>
+
+                    <FaFolderOpen fontSize={30}/>
+                </div>
+
+                <div className={styles.gameContainer}>
+                    <figure className={styles.gameCoverContainer}>
+                        <img className={styles.gameImg} src="https://image.api.playstation.com/vulcan/ap/rnd/202009/1710/UUnpHLJqLAIJX5HioswiPuAm.png" alt="game cover" />
+                    </figure>
+
+                    <p>DMC 5</p>
+
+                    <FaFolderOpen fontSize={30}/>
+                </div>
+
+            </div>
         </main>
     );
 };
