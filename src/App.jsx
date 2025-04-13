64172ae84e7c64c3a5baefda0927e0cb4c8292e6
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-04-13 15:54:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { useState } from "react";
 import "./App.css";
+
+import { RouteGuard } from "./components/reusable/routeGuard/RouteGuard";
+import { useLocalStorage } from "./hooks/useLocalStorage";
+import { AuthContext } from "./contexts/AuthContext";
 
 import Home from "./components/pages/home/Home";
 import Login from "./components/pages/login/Login";
@@ -10,11 +15,8 @@ import Navbar from "./components/reusable/navbar/Navbar";
 import CreateGame from "./components/pages/createGame/CreateGame";
 import Profile from "./components/pages/profile/Profile";
 import PageNotFound from "./components/pages/notFound/PageNotFound";
+import Details from "./components/pages/details/Details";
 
-import { RouteGuard } from "./components/reusable/routeGuard/RouteGuard";
-import { useLocalStorage } from "./hooks/useLocalStorage";
-import { AuthContext } from "./contexts/AuthContext";
-import { useState } from "react";
 
 function App() {
     const [auth, setAuth] = useLocalStorage('auth', {});
@@ -42,6 +44,7 @@ function App() {
                         <Route path="/create" element={<CreateGame />} />
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<Register />} />
+                        <Route path="/details" element={<Details />} />
                     {/* </Route> */}
                 </Routes>
                 <ToastContainer position="bottom-right"/>
