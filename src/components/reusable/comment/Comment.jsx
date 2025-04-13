044a488010bd84cf2d4a3e139d5ef9671bd90d0b
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-04-13 21:50:12 +0300

    Update Comment.jsx

--- src/components/reusable/comment/Comment.jsx
+++ src/components/reusable/comment/Comment.jsx
@@ -1,19 +1,22 @@
+import { useState } from "react";
 import styles from "./Comment.module.css";
 
 import { TimeAgo } from "./TimeAgo";
 
-const Comment = () => {
+
+const Comment = ({ comment, depth = 0 }) => {
+    const [show, setShow] = useState(true);
     return (
-        <>
-            <div className={styles.comment}>
+        <div style={{ marginLeft: depth * 20 + 'px', marginTop: '1rem' }}>
+            <div className={styles.comment} >
                 <div className={styles.commentInfo}>
 
                     <figure>
                         <img className={styles.userImg} src="/images/default-img-pic.webp" alt="User img"  width={50} height={50}/>
                     </figure>
 
-                    <p>User name</p>
-
+                    <p>{comment.author}</p>
+                  
                     <div className={styles.commentBtnContainer}>
                         <button className={styles.commentBtn}>Update</button>
                         <button className={styles.commentBtn}>Delete</button>
@@ -22,55 +25,22 @@ const Comment = () => {
 
                     <TimeAgo />
                 </div>
+
                 <div className={styles.commentText}>
-                    <p>Comment reply text message</p>
+                    <p>{comment.text}</p>
                 </div>
+                
+                <button className={styles.repliesBtn} onClick={(e) => setShow(!show)}>Replies ({comment.replies.length})</button>
             </div>
 
-            <div className={styles.comment}>
-                <div className={styles.commentInfo}>
-
-                    <figure>
-                        <img className={styles.userImg} src="/images/default-img-pic.webp" alt="User img"  width={50} height={50}/>
-                    </figure>
-
-                    <p>User name</p>
-
-                    <div className={styles.commentBtnContainer}>
-                        <button className={styles.commentBtn}>Update</button>
-                        <button className={styles.commentBtn}>Delete</button>
-                        <button className={styles.commentBtn}>Reply</button>
-                    </div>
-
-                    <TimeAgo />
+            {show && 
+                <div>
+                    {comment.replies.map((reply) => (
+                        <Comment key={reply.id} comment={reply} depth={depth + 1} />
+                    ))}
                 </div>
-                <div className={styles.commentText}>
-                    <p>Comment reply text message</p>
-                </div>
-            </div>
-        
-            <div className={styles.comment}>
-                <div className={styles.commentInfo}>
-
-                    <figure>
-                        <img className={styles.userImg} src="/images/default-img-pic.webp" alt="User img"  width={50} height={50}/>
-                    </figure>
-
-                    <p>User name</p>
-
-                    <div className={styles.commentBtnContainer}>
-                        <button className={styles.commentBtn}>Update</button>
-                        <button className={styles.commentBtn}>Delete</button>
-                        <button className={styles.commentBtn}>Reply</button>
-                    </div>
-
-                    <TimeAgo />
-                </div>
-                <div className={styles.commentText}>
-                    <p>Comment reply text message</p>
-                </div>
-            </div>
-        </>
+            }
+        </div>
     );
 };
 
